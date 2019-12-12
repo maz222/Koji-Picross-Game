f5eb2162857d6171a19f6ead31c72c3a9451b051
seane <seane@koji-projects.com>
2019-12-12 18:57:24 +0000

    more updates

--- frontend/Components/App.js
+++ frontend/Components/App.js
@@ -2,49 +2,16 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import Koji from '@withkoji/vcc';
 
-const Container = styled.div`
-    background-color: ${() => Koji.config.colors.backgroundColor};
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: ${() => Koji.config.colors.textColor};
-    text-align: center;
-`;
-
-const AppLogoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const Content = styled.div`
-  padding-bottom: 8px;
-`;
-
-const Icon = styled.div`
-    animation: ${AppLogoSpin} infinite 20s linear;
-    height: 50vmin;
-    width: 50vmin;
-    pointer-events: none;
-    background-image: url(${() => Koji.config.images.icon});
-    background-size: contain;
-    background-repeat: no-repeat;
-    margin-bottom: 16px;
-`;
-
 class App extends React.Component {
+  state = {
+    view: Koji.config.template.showSplashScreen ? 'splash' : 'game',
+  };
+
   render() {
     return (
-      <Container>
-        <h1>{Koji.config.strings.title}</h1>
-        <Icon />
-      </Container>
+      <div>
+        <h1>{this.state.view}</h1>
+      </div>
     );
   }
 }
